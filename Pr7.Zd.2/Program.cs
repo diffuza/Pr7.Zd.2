a6a3d5c4113329d25db2f554aecabc3572972de6
USER <USER@DESKTOP-TRQG01D>
2022-10-10 20:07:25 +0300

    Добавьте файлы проекта.

---
+++ Pr7.Zd.2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labr1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("s= ");
+            float s = float.Parse(Console.ReadLine());
+            double r = Math.Sqrt(s / Math.PI);
+            Console.WriteLine("r= Радиус окружности составляет:{0:N4}", r);
+        }
+    }
+}
